345b5d43fc1ff1fdefef2d1e311a15771984439b
Priyanshu Verma <p27471770@gmail.com>
2025-08-03 12:30:21 +0530

    added logging

--- function.py
+++ function.py
@@ -1,11 +1,18 @@
 import os, pathlib, shutil, json
-import datetime
+# import datetime
+import logging as log
+
+log.basicConfig(
+    filename="foldercleaner.log",
+    level=log.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
 class Sorter:
     def __init__(self, entry, result_label):
         self.entry = entry
         self.result_label = result_label
-        print(os.getcwd()) 
+        log.info(f"Sorter initialized with entry and result_label on path {self.entry.get()}")
 
     def sort(self):
         global check
@@ -39,30 +46,39 @@ class Sorter:
                     destination = os.path.join(source_directory, common_extensions[extension])
                     # Make the folder if it does not exist
                     if not os.path.exists(destination):
+                        log.info(f"Creating directory {destination} for extension {extension}")
                         os.makedirs(destination)
                         shutil.move(source_file, destination)
+                        # log.info(f"Moved {source_file} to {destination}")
                 # Move the file if it is mapped
                     else:
                         try:
                             if i not in os.listdir(destination):
                                 shutil.move(source_file, destination)
+                                # log.info(f"Moved {source_file} to {destination}")
                             else:
                                 os.remove(source_file)
+                                log.warning(f"Repeated file found: {source_file}")
                         except:
                             current = self.result_label.get()
                             self.result_label.set("repeated file found")
                             print(source_file, "----")
+
                 else:
                     continue
-                print(f"Moved {i} to {destination}")
+                # print(f"Moved {i} to {destination}")
+                log.info(f"Moved {source_file} to {destination}")
 
             if check == 0:
                 current = self.result_label.get()
                 self.result_label.set("The present entities are a folder or not have been mapped")
+                log.warning(f"Unmapped files found in {source_directory}: {list_of_files}")
             else:
                 current = self.result_label.get()
             self.result_label.set("Successful All files which can be mapped are moved")
+            log.info(f"All files which can be mapped are moved from {source_directory}")
         except:
             current = self.result_label.get()
             self.result_label.set("Invalid Input")
-        print(source_file, "----")
+            log.error(f"Invalid input for source directory: {self.entry.get()}")
+        # print(source_file, "----")
